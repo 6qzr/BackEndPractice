d85f8d5dc2f72318beaff8260b70a1d41306dcea
Mohammed Al Abri <striker.m901@gmail.com>
2026-06-30 15:51:04 +0400

    Update Course class to public and add course properties
    Changed Course class from internal to public for wider access. Added properties for courseId, courseCode, courseTitle, creditHours, departmentId, instructorId (nullable), and semesterOffered to represent course attributes and relationships.

--- UniversitySystem/Models/Course.cs
+++ UniversitySystem/Models/Course.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace UniversitySystem.Models
 {
-    internal class Course
+    public class Course
     {
+        public  int     courseId        { get; set; }   // System Generated
+        public  string  courseCode      { get; set; }   // User Input
+        public  string  courseTitle     { get; set; }   // User Input
+        public  int     creditHours     { get; set; }   // User Input
+        public  int     departmentId    { get; set; }   // Foreign Key
+        public  int?    instructorId    { get; set; }   // Foreign Key
+        public  string  semesterOffered { get; set; }   // User Input
     }
 }
