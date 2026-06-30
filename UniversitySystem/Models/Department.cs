25927435ff0790a64494eb11be3b436f39effe1a
Mohammed Al Abri <striker.m901@gmail.com>
2026-06-30 17:21:33 +0400

    Enhance Department model with EF Core and validation attributes
    Added data annotations and EF Core attributes to the Department class for improved validation and database mapping. Introduced unique index on departmentName, required fields, max length constraints, value ranges, and foreign key relationships to Instructor and Course entities. Updated property definitions and imported necessary namespaces.

--- UniversitySystem/Models/Department.cs
+++ UniversitySystem/Models/Department.cs
@@ -1,15 +1,33 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace UniversitySystem.Models
 {
+    [Index(nameof(departmentName), IsUnique = true)]
     public class Department
     {
+        [Key]
         public  int     departmentId        { get; set; }   // System Generated
+
+        [Required]
+        [MaxLength(100)]
         public  string  departmentName      { get; set; }   // User Input
+
+        [MaxLength(50)]
         public  string  building            { get; set; }   // User Input
+
+        [Required]
+        [Range(0, double.MaxValue)]
         public  decimal budget              { get; set; }   // User Input
+
+        [ForeignKey("Instructor")]
         public  int?    headInstructorId    { get; set; }   // Foreign Key
+        public  virtual Instructor Instructor { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
     }
 }
