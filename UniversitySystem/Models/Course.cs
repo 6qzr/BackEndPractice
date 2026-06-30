74e9881fb2f69ba1f846248f32f409bc6c2d215f
Mohammed Al Abri <striker.m901@gmail.com>
2026-06-30 17:09:35 +0400

    Refactor Course class with EF Core attributes and props
    Refactored and expanded the Course class to use Entity Framework Core data annotations for validation and relationships. Added properties for course details, foreign keys, navigation properties, and enforced unique courseCode with an index.

--- UniversitySystem/Models/Course.cs
+++ UniversitySystem/Models/Course.cs
@@ -1,17 +1,44 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+
 
 namespace UniversitySystem.Models
 {
+    [Index(nameof(courseCode), IsUnique = true)]
+    
     public class Course
     {
+        [Key]
         public  int     courseId        { get; set; }   // System Generated
+
+        [Required]
+        [MaxLength(10)]
         public  string  courseCode      { get; set; }   // User Input
+
+        [Required]
+        [MaxLength(150)]
         public  string  courseTitle     { get; set; }   // User Input
+
+        [Required]
+        [Range(1, 6)]
         public  int     creditHours     { get; set; }   // User Input
+
+        [ForeignKey("Department")]
         public  int     departmentId    { get; set; }   // Foreign Key
+        public  virtual Department Department { get; set; }
+
+        [ForeignKey("Instructor")]
         public  int?    instructorId    { get; set; }   // Foreign Key
+        public  virtual Instructor Instructor { get; set; }
+
+        [Required]
+        [MaxLength(20)]
         public  string  semesterOffered { get; set; }   // User Input
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
