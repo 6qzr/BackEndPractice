1bc963c4e106da6fd0ed06e74e1d6b153b8973cf
Mohammed Al Abri <striker.m901@gmail.com>
2026-06-30 16:54:06 +0400

    Enhance Enrollment model with data annotations and navigation
    Added validation and database mapping attributes to Enrollment class. Introduced navigation properties for Student and Course. Updated status property to be required with a default value and max length. Made finalGrade nullable with a max length. Improved comments and property consistency.

--- UniversitySystem/Models/Enrollment.cs
+++ UniversitySystem/Models/Enrollment.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace UniversitySystem.Models
 {
     public class Enrollment
     {
+        [Key]
         public  int         enrollmentId    { get; set; }   // System Generated
+        
+        [ForeignKey("Student")]
         public  int         studentId       { get; set; }   // Foreign Key
+        public  virtual Student Student     { get; set; }
+        
+        [ForeignKey("Course")]
         public  int         courseId        { get; set; }   // Foreign Key
+        public  virtual     Course Course   { get; set; }
+
+        [Required]
         public  DateTime    enrollmentDate  { get; set; }   // User Input
+        
+        [MaxLength(2)]
         public  string?     finalGrade      { get; set; }   // User Input
-        public  string      status          { get; set; }   // Default Value
+        
+        [Required]
+        [MaxLength(20)]
+        public  string      status          { get; set; } = "In Progress";    // Default Value
     }
 }
