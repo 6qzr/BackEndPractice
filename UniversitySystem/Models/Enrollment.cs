6d5e4f4fb0152e310bb1b497a539f8e48444b978
Mohammed Al Abri <striker.m901@gmail.com>
2026-06-30 16:41:11 +0400

    Update Enrollment class to public and add properties
    Changed Enrollment class visibility from internal to public. Added properties for enrollmentId, studentId, courseId, enrollmentDate, finalGrade, and status with relevant data types and comments.

--- UniversitySystem/Models/Enrollment.cs
+++ UniversitySystem/Models/Enrollment.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace UniversitySystem.Models
 {
-    internal class Enrollment
+    public class Enrollment
     {
+        public  int         enrollmentId    { get; set; }   // System Generated
+        public  int         studentId       { get; set; }   // Foreign Key
+        public  int         courseId        { get; set; }   // Foreign Key
+        public  DateTime    enrollmentDate  { get; set; }   // User Input
+        public  string?     finalGrade      { get; set; }   // User Input
+        public  string      status          { get; set; }   // Default Value
     }
 }
