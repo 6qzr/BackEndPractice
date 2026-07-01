0fe4fe091be1c28e8b8f45b0fc8232d35bf96fbf
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-01 16:38:48 +0400

    Add properties to Order class for order details
    Expanded the Order class by adding properties for orderId, userId, orderDate, totalAmount (with default 0), status (with default "Pending"), shippingAddress, and paymentMethod to capture essential order information.

--- E-CommerceSystem/Models/Order.cs
+++ E-CommerceSystem/Models/Order.cs
@@ -6,5 +6,12 @@ namespace E_CommerceSystem.Models
 {
     public class Order
     {
+        public  int         orderId         { get; set; }   // System Generated
+        public  int         userId          { get; set; }   // Foreign Key
+        public  DateTime    orderDate       { get; set; }   // System Generated
+        public  decimal     totalAmount     { get; set; } = 0;  // Calculated
+        public  string      status          { get; set; } = "Pending";   // From List
+        public  string      shippingAddress { get; set; }   // User Input
+        public  string      paymentMethod   { get; set; }   // From List
     }
 }
