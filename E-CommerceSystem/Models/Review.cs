5c608a14d21165cde1e79af25eac016278271ce3
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-01 16:45:51 +0400

    Add properties to Review class for user, product, and rating
    Expanded the Review class by adding properties for reviewId, userId, productId, rating, comment, and reviewDate (defaulting to DateTime.Now) to better represent review details and relationships.

--- E-CommerceSystem/Models/Review.cs
+++ E-CommerceSystem/Models/Review.cs
@@ -6,5 +6,11 @@ namespace E_CommerceSystem.Models
 {
     public class Review
     {
+        public  int         reviewId    { get; set; }   // System Generated
+        public  int         userId      { get; set; }   // Foreign Key
+        public  int         productId   { get; set; }   // Foreign Key
+        public  int         rating      { get; set; }   // User Input
+        public  string      comment     { get; set; }   // User Input
+        public  DateTime    reviewDate  { get; set; } = DateTime.Now  // Default Value
     }
 }
