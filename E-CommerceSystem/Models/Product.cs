00e8f701e68b44296e50431615660b8af54c6bcc
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-01 16:29:05 +0400

    Add properties to Product class for product attributes
    Expanded the Product class with properties such as productId, productName, descriptiopn (typo), price, stockQuantity (default 0), imageUrl, categoryId, createdAt, and isAvailable (default true) to capture essential product details.

--- E-CommerceSystem/Models/Product.cs
+++ E-CommerceSystem/Models/Product.cs
@@ -6,5 +6,14 @@ namespace E_CommerceSystem.Models
 {
     public class Product
     {
+        public  int         productId       { get; set; }   // System Generated
+        public  string      productName     { get; set; }   // User Input
+        public  string      descriptiopn    { get; set; }   // User Input
+        public  decimal     price           { get; set; }   // User Input
+        public  int         stockQuantity   { get; set; } = 0   // Default Value
+        public  string      imageUrl        { get; set; }   // User Input
+        public  int         categoryId      { get; set; }   // Foreign Key
+        public  DateTime    createdAt       { get; set; }   // System Generated
+        public  bool        isAvailable     { get; set; } = true;   // Default Value
     }
 }
