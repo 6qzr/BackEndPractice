72a810e62bc3289461af899c3630c6045b73884c
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-01 16:09:58 +0400

    Add user properties to User class in User.cs
    Added properties for userId, username, email, passwordHash, fullName, phoneNumber, address, registerationDate, and isActive to the User class to store user details and status.

--- E-CommerceSystem/Models/User.cs
+++ E-CommerceSystem/Models/User.cs
@@ -6,5 +6,14 @@ namespace E_CommerceSystem.Models
 {
     public class User
     {
+        public  int         userId              { get; set; }   // System Generated
+        public  string      username            { get; set; }   // User Input
+        public  string      email               { get; set; }   // User Input
+        public  string      passwordHash        { get; set; }   // User Input
+        public  string      fullName            { get; set; }   // User Input
+        public  string      phoneNumber         { get; set; }   // User Input
+        public  string      address             { get; set; }   // User Input
+        public  DateTime    registerationDate   { get; set; }   // System Generated
+        public  bool        isActive            { get; set; } = true;  // Default Value          
     }
 }
