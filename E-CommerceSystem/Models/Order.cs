cc21feb093b4c1cbb4dbed0a388e919baf195ff7
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-02 12:16:34 +0400

    Refactor Order class with EF Core data annotations
    Refactored the Order class to use Entity Framework Core data annotations for improved schema mapping and validation. Added attributes such as [Table], [Key], [DatabaseGenerated], [ForeignKey], [Required], [Range], and [MaxLength]. Defined navigation properties for User and OrderItems, updated default values, and removed unnecessary using directives.

--- E-CommerceSystem/Models/Order.cs
+++ E-CommerceSystem/Models/Order.cs
@@ -1,20 +1,41 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace E_CommerceSystem.Models
 {
+    [Table("Orders")]
     public class Order
     {
-        public  int         orderId         { get; set; }   // System Generated
-        public  int         userId          { get; set; }   // Foreign Key
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int orderId { get; set; }   // System Generated
+
+        [ForeignKey(nameof(User))]
+        public int userId { get; set; }   // Foreign Key
+        public virtual User User { get; set; }  // Naviagtion Property
+
+        [Required]
         public DateTime orderDate { get; set; } = DateTime.Now;   // Default Value
-        public  decimal     totalAmount     { get; set; } = 0;  // Calculated
-        public  string      status          { get; set; } = "Pending";   // From List
-        public  string      shippingAddress { get; set; }   // User Input
-        public  string      paymentMethod   { get; set; }   // From List
 
+        [Required]
+        [Range(0, (double)decimal.MaxValue)]
+        public decimal totalAmount { get; set; } = 0;   // Default Value
 
-        public virtual ICollection<OrderItem> OrderItems { get; set; }  // Navigation Property
+        [Required]
+        [MaxLength(30)]
+        public string status { get; set; } = "Pending";   // default value
+
+        [Required]
+        [MaxLength(300)]
+        public string shippingAddress { get; set; }   // User Input
+
+        [Required]
+        [MaxLength(50)]
+        public string paymentMethod { get; set; }   // From List
+
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
     }
 }
