ea7e017a146ad86ea0c1475173d46618266ce1f4
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-02 12:12:16 +0400

    Update Review model with EF Core data annotations
    Refactored the Review class to use Entity Framework Core data annotations for improved database mapping. Added navigation properties for User and Product, set property constraints, and mapped the class to the "Reviews" table. Cleaned up using directives.

--- E-CommerceSystem/Models/Review.cs
+++ E-CommerceSystem/Models/Review.cs
@@ -1,16 +1,35 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceSystem.Models
 {
+    [Table("Reviews")]
     public class Review
     {
-        public  int         reviewId    { get; set; }   // System Generated
-        public  int         userId      { get; set; }   // Foreign Key
-        public  int         productId   { get; set; }   // Foreign Key
-        public  int         rating      { get; set; }   // User Input
-        public  string      comment     { get; set; }   // User Input
-        public  DateTime    reviewDate  { get; set; } = DateTime.Now  // Default Value
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public  int     reviewId { get; set; }   // System Generated
+
+        [ForeignKey(nameof(User))]
+        public  int     userId { get; set; }   // Foreign Key
+        public  virtual User User { get; set; }  // Navigation Property
+
+        [ForeignKey(nameof(Product))]
+        public  int     productId { get; set; }   // Foreign Key
+        public  virtual Product Product { get; set; }    // Navigation Property
+
+        [Required]
+        [Range(1, 5)]
+        public  int     rating { get; set; }   // User Input
+
+        [MaxLength(1000)]
+        public  string? comment { get; set; }   // User Input
+
+        [Required]
+        public  DateTime    reviewDate { get; set; } = DateTime.Now;   // Default Value
     }
 }
