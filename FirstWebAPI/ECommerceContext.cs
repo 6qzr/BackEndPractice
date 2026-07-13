373f47315aef6426e98e1b4aba80dcbb6842e3f0
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-13 15:23:30 +0400

    Add ECommerceContext with DbSets for core entities
    Introduced ECommerceContext class inheriting from DbContext to manage Users, Categories, Products, Orders, OrderItems, and Reviews. Configured context for dependency injection using DbContextOptions. Enables EF Core operations on main e-commerce entities.

---
+++ FirstWebAPI/ECommerceContext.cs
@@ -0,0 +1,19 @@
+﻿using E_CommerceSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceSystem
+{
+    public class ECommerceContext   :   DbContext
+    {
+        public DbSet<User>      Users       { get; set; }
+        public DbSet<Category>  Categories  { get; set; }
+        public DbSet<Product>   Products    { get; set; }
+        public DbSet<Order>     Orders      { get; set; }
+        public DbSet<OrderItem> OrderItems  { get; set; }
+        public DbSet<Review>    Reviews     { get; set; }
+
+        public ECommerceContext(DbContextOptions<ECommerceContext> options) : base(options)
+        {
+        }
+    }
+}
